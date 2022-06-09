88199dbf7222c7639ae4dfc63c8522e040ec673b
gethcv <107171122+gethcv@users.noreply.github.com>
2022-06-09 12:07:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.header("My parents new healthy diner")
 
 streamlit.text("Omega 3 & Blue berry oat meal")
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
